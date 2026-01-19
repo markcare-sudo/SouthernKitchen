7e87ca707ce3a9ecd686a9cecae3fecdcf2287af
Bharath136 <bk3604073@gmail.com>
2026-01-19 17:14:25 +0530

    added google search console

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -3,8 +3,6 @@ import { lazy } from "react";
 import MainLayout from "../layouts/MainLayout";
 import ErrorBoundary from "../components/ErrorBoundory";
 import NotFound from "@/pages/NotFound";
-import AboutUs from "@/pages/AboutUs";
-import Contact from "@/pages/ContactUs";
 
 // Lazy-loaded pages
 const Home = lazy(() => import("../pages/Home"));
@@ -24,27 +22,6 @@ const AppRoutes = () => {
             </ErrorBoundary>
           }
         />
-
-
-        <Route
-          path="/about-us"
-          element={
-            <ErrorBoundary>
-              <AboutUs />
-            </ErrorBoundary>
-          }
-        />
-
-        <Route
-          path="/contact-us"
-          element={
-            <ErrorBoundary>
-              <Contact />
-            </ErrorBoundary>
-          }
-        />
-
-
 
         <Route
           path="*"
